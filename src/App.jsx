2a164211a998805769a4a37a27c9999d7be777ae
hani <guptahani75@gmail.com>
2026-06-26 16:49:01 +0530

    feat: added routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,17 @@
-import Navbar from "./components/Navbar";
-import Hero from "./components/Hero";
-import About from "./components/About";
-import Skills from "./components/Skills";
-import Projects from "./components/Projects";
-import OpenSource from "./components/OpenSource";
-import Contact from "./components/Contact";
-import Footer from "./components/Footer";
-import Leetcode from "./components/Leetcode";
-import Stats from "./components/Stats";
-import Education from "./components/Education";
-import Particles from "./components/Particles";
+import { Routes, Route } from "react-router-dom";
 
-import "./App.css";
+import Home from "./pages/Home";
+import Contributions from "./pages/Contributions";
 
 function App() {
   return (
-    <div className="app">
-      <Particles />
-      <Navbar />
-      <Hero />
-      <Stats />
-      <About />
-      <Skills />
-      <Projects />
-      <OpenSource />
-      <Leetcode />
-      <Education />
-      <Contact />
-      <Footer />
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route
+        path="/contributions"
+        element={<Contributions />}
+      />
+    </Routes>
   );
 }
 
