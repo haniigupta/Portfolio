b4d870c25b8e7f03984518fd5a4b7736c1480693
hani <guptahani75@gmail.com>
2026-06-27 16:21:58 +0530

    created: routes for portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 
 import Home from "./pages/Home";
-import Contributions from "./pages/Contributions";
+import Contributions from "./pages/OpenSourceExperience";
 
 function App() {
   return (
