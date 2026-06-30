dff0fda053d561e710d1a0b73eb3ffa2ff52e170
guptahani75@gmail.com <guptahani75@gmail.com>
2026-06-30 23:09:56 +0530

    feat: fix tailwind to file

--- src/components/Achievements.jsx
+++ src/components/Achievements.jsx
@@ -2,7 +2,7 @@ function Achievements() {
   const achievements = [
     {
       title: "LeetCode",
-      value: "160+ Problems",
+      value: "200+ Problems",
     },
     {
       title: "Open Source",
@@ -19,15 +19,21 @@ function Achievements() {
   ];
 
   return (
-    <section>
-      <h2 className="section-title">
+    <section className="px-[10%] py-[100px]">
+      <h2 className="mb-[30px] text-5xl max-md:text-[2.2rem]">
         Highlights
       </h2>
 
-      <div className="stats-grid">
+      <div className="grid grid-cols-4 gap-[25px] max-md:grid-cols-2 max-[480px]:grid-cols-1">
         {achievements.map((item, i) => (
-          <div className="stat-card" key={i}>
-            <h2>{item.value}</h2>
+          <div
+            key={i}
+            className="rounded-[20px] bg-white/5 p-[30px] text-center backdrop-blur-[15px] transition duration-300 hover:-translate-y-[10px]"
+          >
+            <h2 className="text-5xl text-[#6d5dfc]">
+              {item.value}
+            </h2>
+
             <p>{item.title}</p>
           </div>
         ))}
