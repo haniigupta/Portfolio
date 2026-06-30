c26ad5f8c09eed5a4150559e3afe83abe543be1a
guptahani75@gmail.com <guptahani75@gmail.com>
2026-06-30 22:46:47 +0530

    added certificates png in the page

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import { Routes, Route } from "react-router-dom";
 
 import Home from "./pages/Home";
 import Contributions from "./pages/OpenSourceExperience";
+import Certificate from "./pages/Certificate";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/certificates" element={<Certificate />} />
       <Route
         path="/contributions"
         element={<Contributions />}
