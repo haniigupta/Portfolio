7865f7c086caa2679d606b1f7b440ab782c91db9
guptahani75@gmail.com <guptahani75@gmail.com>
2026-06-30 23:39:27 +0530

    fix: 160 to 200 ques

--- src/components/Leetcode.jsx
+++ src/components/Leetcode.jsx
@@ -2,29 +2,55 @@ import { FaCode, FaBrain, FaLaptopCode } from "react-icons/fa";
 
 function Leetcode() {
   return (
-    <section id="leetcode">
-      <h2 className="section-title">DSA Journey</h2>
+    <section
+      id="leetcode"
+      className="px-[10%] py-[100px]"
+    >
+      <h2 className="mb-[55px] text-[3rem] max-md:text-[2.2rem]">
+        DSA Journey
+      </h2>
 
-      <div className="dsa-grid">
-        <div className="dsa-card">
-          <FaCode />
-          <h3>160+</h3>
-          <p>Problems Solved</p>
+      <div className="grid grid-cols-1 gap-8 lg:grid-cols-3">
+        <div
+          className="flex min-h-[320px] flex-col items-center justify-center rounded-[28px] border border-white/[0.08] bg-white/5 p-10 text-center
+          backdrop-blur-[20px] transition-all duration-300 hover:-translate-y-2 hover:shadow-[0_0_30px_rgba(109,93,252,.2)] "
+        >
+          <FaCode className="mb-8 text-[3rem] text-[#6d5dfc]" />
+
+          <h3 className="mb-5 text-[2rem] font-bold">
+            200+
+          </h3>
+
+          <p className="leading-7 text-[#c7c7c7]">Problems Solved</p>
         </div>
 
-        <div className="dsa-card">
-          <FaBrain />
-          <h3>Core Topics</h3>
-          <p>
+       <div
+          className="flex min-h-[320px] flex-col items-center justify-center rounded-[28px] border border-white/[0.08] bg-white/5 p-10 text-center
+          backdrop-blur-[20px] transition-all duration-300 hover:-translate-y-2 hover:shadow-[0_0_30px_rgba(109,93,252,.2)] "
+        >
+          <FaBrain className="mb-8 text-[3rem] text-[#6d5dfc]" />
+
+          <h3 className="mb-5 text-[2rem] font-bold">
+            Core Topics
+          </h3>
+
+          <p className="leading-7 text-[#c7c7c7]">
             Arrays, Trees, Graphs, DP,
             Linked Lists, Recursion
           </p>
         </div>
 
-        <div className="dsa-card">
-          <FaLaptopCode />
-          <h3>Platform</h3>
-          <p>LeetCode</p>
+        <div
+          className="flex min-h-[320px] flex-col items-center justify-center rounded-[28px] border border-white/[0.08] bg-white/5 p-10 text-center
+          backdrop-blur-[20px] transition-all duration-300 hover:-translate-y-2 hover:shadow-[0_0_30px_rgba(109,93,252,.2)] "
+        >
+          <FaLaptopCode className="mb-8 text-[3rem] text-[#6d5dfc]" />
+
+          <h3 className="mb-5 text-[2rem] font-bold">
+            Platform
+          </h3>
+
+          <p className="leading-7 text-[#c7c7c7]">LeetCode</p>
         </div>
       </div>
     </section>
